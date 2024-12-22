e09eacc89fbb1bdf790e17568c72bc7a21651504
brucewayne <manivarshithpc@gmail.com>
2024-12-22 11:50:18 +0530

    Time: 0 ms (100.00%) | Memory: 63.4 MB (40.09%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+       int start=0 , end=arr.size()-1 , mid;
+
+       while(start<=end){
+        mid =end+(start-end)/2;
+
+        //peak element 
+        if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+            return mid;
+        }
+        //move right side
+        else if(arr[mid]>arr[mid-1]){
+            start=mid+1;
+            
+        }
+        else 
+          end=mid-1;
+
+       }
+       return -1;
+
+        
+    }
+};
