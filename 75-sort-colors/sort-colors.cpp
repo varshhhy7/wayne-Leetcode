ac31ca34e8cd34cd31aa3270f32652da9868e73d
Varshith <manivarshithpc@gmail.com>
2024-12-29 12:36:18 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (17.31%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,22 +1,26 @@
-class Solution {
-public:
-    void sortColors(vector<int>& arr) {
-        int start=0 , end =arr.size()-1;
-        for(int i=0 ; i<=end ;){
-            if(arr[i]==0){
-                swap(arr[i] , arr[start]);
-                start++;
-                i++;
+class Solution
+{
+    public:
+        void sortColors(vector<int> &arr)
+        {
+            int start = 0, end = arr.size() - 1;
+            for (int i = 0; i <= end;)
+            {
+                if (arr[i] == 0)
+                {
+                    swap(arr[i], arr[start]);
+                    start++;
+                    i++;
+                }
+                else if (arr[i] == 2)
+                {
+                    swap(arr[i], arr[end]);
+                    end--;
+                }
+                else
+                {
+                    i++;
+                }
             }
-        
-        else if(arr[i]==2){
-            swap(arr[i],arr[end]);
-            end--;
         }
-        else{
-            i++;
-        }
-        }
-       
-    }
 };
