b351018eae3561096cedc1eca450da1157f2ba20
Varshith <manivarshithpc@gmail.com>
2024-12-29 13:54:01 +0530

    Time: 71 ms (19.30%) | Memory: 14 MB (73.63%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+    public:
+        vector<int> twoSum(vector<int> &arr, int target)
+        {
+            vector<int> ans;
+
+            for (int i = 0; i < arr.size(); i++)
+            {
+                for (int j = i + 1; j < arr.size(); j++)
+                {
+                    if (arr[i] + arr[j] == target)
+                    {
+                        ans.push_back(i);
+                        ans.push_back(j);
+                        return ans;
+                    }
+                }
+            }
+
+            return ans;
+        }
+};
