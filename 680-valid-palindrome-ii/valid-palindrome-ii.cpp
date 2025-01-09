5471d4f342ad5173be9aa32f07e971bb96844868
Varshith <manivarshithpc@gmail.com>
2025-01-09 20:10:35 +0530

    Time: 8 ms (27.19%) | Memory: 26.7 MB (29.02%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // Helper function to check if a substring is a palindrome
+    
     bool isValidPalindrome(string s, int l, int h) {
         while (l < h) {
             if (s[l] != s[h])
@@ -16,12 +16,12 @@ public:
 
         while (l < h) {
             if (s[l] != s[h]) {
-                // Try skipping one character (either l or h)
+               
                 return isValidPalindrome(s, l + 1, h) || isValidPalindrome(s, l, h - 1);
             }
             l++;
             h--;
         }
-        return true; // The string is already a palindrome
+        return true; 
     }
 };
